8ef7c22e3605d5287f7bb6f5ce7aa070420f46d1
Igor Loredo <igorloredo12@gmail.com>
2023-11-20 00:42:31 -0300

    logion funcioando perfeitamente

--- src/componentes/Login/Login.js
+++ src/componentes/Login/Login.js
@@ -1,84 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import CampoTexto from '../CampoTexto/CampoTexto';
-import Botao from '../Botao/Botao';
-import './Login.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Alerts from "../Alert/Alerts";
+import Botao from "../Botao/Botao";
+import "./Login.css";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Login = ({ onLogin }) => {
-    const [previousPath, setPreviousPath] = useState('');
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [previousPath, setPreviousPath] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        setPreviousPath(location.state?.from || '/welcome');
-    }, [location]);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-    // const handleLogin = () => {
-    //     // Simulação de login bem-sucedido
-    //     console.log("Login realizado com sucesso");
-    //     navigate(previousPath);
-    // };
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post('http://localhost:5001/api/login', {
-                username: 'username_value',
-                password: 'password_value'
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-                }
-            });
-            console.log(response.status);
-            if (response.status === 200) {
-                console.log("Login realizado com sucesso");
-                navigate(previousPath);
-            } else {
-                console.log("Login falhou");
-            }
-        } catch (error) {
-            console.error("Erro ao fazer login", error);
+  useEffect(() => {
+    setPreviousPath(location.state?.from || "/welcome");
+  }, [location]);
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      console.log(username);
+      console.log(password);
+      if (!username || !password) {
+        window.alert('Por favor, preencha todos os campos.');
+        return;
+      }
+      const response = await axios.post(
+        "http://localhost:5001/api/login",
+        {
+          username: username,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          },
         }
-    };
-    
-      
-    return(
-        <div> 
-        <div className="container">
+      );
+      console.log(response.status);
+      if (response.status === 200) {
+        console.log("Login realizado com sucesso");
+        navigate(previousPath);
+      } 
+      else {
+        console.log("Login falhou");
+        window.alert('An error occurred while logging in.');
+      }
+    } catch (error) {
+       if (error.response.status === 401){
+            window.alert('Usuário ou senha incorretos.');
+          }
+      console.error("Erro ao fazer login", error);
+    }
+  };
 
-            <div className="content">
-                <img src="/imagens/enchente2.jpg" className="left-image"/>
-                <div class="text-block">
-                    <h1>
-                        Login
-                    </h1>
-                    
-                    <CampoTexto label="Número de usuário" 
-                    placeholder="Digite o número de usuário"
-                    />
+  return (
+    <div>
+      <div className="container">
+        <div className="content">
+          <img src="/imagens/enchente2.jpg" className="left-image" />
+          <div class="text-block">
+            <h1>Login</h1>
 
-                    <CampoTexto label="Senha" 
-                    placeholder="Digite a senha"
-                    />
-                    <div className="login">
-                         <button onClick={handleLogin}>Login</button>
-                    </div>
-
-                    <br></br>
-
-                    <p>É seu primeiro acesso?</p>
-
-                    <Botao text="Criar conta"/>
-
-                </div>
+            <div className="campo-texto">
+              <label>Número de usuário</label>
+              <input
+                placeholder="Digite o número de usuário"
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+              ></input>
             </div>
-        </div>
-        </div>
-        
-    );
-}
+            <div className="campo-texto">
+              <label>Senha</label>
+              <input
+                placeholder="Digite a senha"
+                type="password"
+                value={password}
+                required
+                onChange={handlePasswordChange}
+              ></input>
+            </div>
 
-export default Login
+            <div className="login">
+              <button className="botao-login" type="submit" onClick={handleLogin}>
+                Login
+              </button>
+            </div>
+
+            <br></br>
+
+            <p>É seu primeiro acesso?</p>
+
+            <Botao text="Criar conta" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
